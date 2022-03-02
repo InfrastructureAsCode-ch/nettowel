92ddb1e1016243bd9c4d9ba89eb8c60ad8782011
ubaumann <github@m.ubaumann.ch>
2022-03-02 08:56:23 +0100

    Add TTP render

--- nettowel/cli/ttp.py
+++ nettowel/cli/ttp.py
@@ -1,8 +1,16 @@
+import sys
 import typer
+from typing import List
 from rich import inspect as rich_inspect
-from rich import print_json
+from rich import print_json, print
 
-from nettowel.cli._common import get_members, cleanup_dict
+from rich.json import JSON
+from rich.panel import Panel
+from rich.columns import Columns
+from rich.syntax import Syntax
+
+from nettowel.yaml import load as yaml_load
+from nettowel.ttp import render_template
 
 
 app = typer.Typer(help="TTP templating functions")
@@ -11,15 +19,107 @@ app = typer.Typer(help="TTP templating functions")
 @app.command()
 def render(
     ctx: typer.Context,
-    # demo: str,
+    template_file_name: typer.FileText = typer.Argument(
+        ...,
+        exists=True,
+        file_okay=True,
+        dir_okay=False,
+        readable=True,
+        resolve_path=True,
+        allow_dash=True,
+        metavar="TEMPLATE",
+    ),
+    data_file_name: typer.FileText = typer.Argument(
+        ...,
+        exists=True,
+        file_okay=True,
+        dir_okay=False,
+        readable=True,
+        resolve_path=True,
+        allow_dash=True,
+        metavar="DATA",
+    ),
     json: bool = typer.Option(default=False, help="json output"),
+    only_result: bool = typer.Option(False, "--print-result-only", help="Raw output"),
+    floating_panels: bool = typer.Option(
+        False,
+        "--floating-panels",
+        help="Adjust the width of the panel to fit the content",
+    ),
 ) -> None:
     try:
-        raise NotImplementedError("Not Implemented Yet")
-        if json:
-            print_json(data=None)
+        if template_file_name == "-":
+            template_text = sys.stdin.read()
         else:
-            rich_inspect(None)
+            with open(template_file_name) as template_file:  # type: ignore
+                template_text = template_file.read()
+        if data_file_name == "-":
+            input_data = sys.stdin.read()
+        else:
+            with open(data_file_name) as data_file:  # type: ignore
+                input_data = data_file.read()
+
+        result = render_template(template=template_text, data=input_data)
+        if json:
+            print_json(data=result)
+
+        else:
+            result_output = JSON.from_data(result)
+            if floating_panels:
+                code_width = (
+                    max(
+                        [len(x) for x in template_text.split("\n")]
+                        + [len(x) for x in result_output.text.plain.split("\n")]
+                    )
+                    + 6
+                )
+            else:
+                code_width = None
+            panels: List[Panel] = []
+            if not only_result:
+                data_output = Syntax(
+                    input_data,
+                    "jinja",
+                    line_numbers=True,
+                    indent_guides=True,
+                    code_width=code_width,
+                )
+                template_output = Syntax(
+                    template_text,
+                    "jinja",
+                    line_numbers=True,
+                    indent_guides=True,
+                    code_width=code_width,
+                )
+                panels.append(
+                    Panel(
+                        template_output, title="[yellow]Template", border_style="blue"
+                    )
+                )
+                panels.append(
+                    Panel(data_output, title="[yellow]Data", border_style="blue")
+                )
+            panels.append(
+                Panel(
+                    result_output,
+                    border_style="blue",
+                    title="[yellow]Result",
+                )
+            )
+            if not only_result and floating_panels:
+                height = (
+                    max(
+                        [
+                            len(result_output.text.plain.split("\n")),
+                            len(input_data.split("\n")),
+                            len(template_text.split("\n")),
+                        ]
+                    )
+                    + 2
+                )
+                for p in panels:
+                    p.height = height
+            print(Columns(panels, equal=True))
         typer.Exit(0)
 
     except NotImplementedError as exc:
--- nettowel/ttp.py
+++ nettowel/ttp.py
@@ -1,3 +1,5 @@
+from typing import Any
+
 try:
     from ttp import ttp
 
@@ -7,3 +9,17 @@ except ImportError:
     TTP_INSTALLED = False
 
 NOT_INSTALLED = "TTP is not installed. Install it with pip: pip install nettowel[ttp]"
+
+
+def render_template(
+    template: str,
+    data: str,
+) -> Any:
+    try:
+        parser = ttp(data=data, template=template)
+        parser.parse()
+
+        results = parser.result()[0]
+    except Exception as exc:
+        raise exc
+    return results
