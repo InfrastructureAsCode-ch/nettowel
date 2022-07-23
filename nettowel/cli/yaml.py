114f5a1170da3d702a8b00c1de8631cbdc47f3ca
ubaumann <github@m.ubaumann.ch>
2022-07-23 19:25:09 +0200

    Add yaml cli featrue

--- nettowel/cli/yaml.py
+++ nettowel/cli/yaml.py
@@ -1,50 +1,86 @@
 import typer
 from rich import inspect as rich_inspect
-from rich import print_json
+from rich import print_json, print
+from rich.panel import Panel
+from rich.syntax import Syntax
+from rich.json import JSON
 
-from nettowel.cli._common import get_members, cleanup_dict, get_typer_app
+from nettowel.cli._common import (
+    auto_complete_paths,
+    read_yaml,
+    cleanup_dict,
+    get_typer_app,
+)
+from nettowel.yaml import dump as yaml_dump
 
-
-app = get_typer_app(help="Templating (Jinja2) functions")
+app = get_typer_app(help="YAML functions")
 
 
 @app.command()
 def load(
     ctx: typer.Context,
-    # demo: str,
-    json: bool = typer.Option(default=False, help="json output"),
+    data_file_name: typer.FileText = typer.Argument(
+        ...,
+        exists=True,
+        file_okay=True,
+        dir_okay=False,
+        readable=True,
+        resolve_path=True,
+        allow_dash=True,
+        metavar="YAML",
+        help="YAML Data",
+        autocompletion=auto_complete_paths,
+    ),
+    json: bool = typer.Option(False, "--json", help="JSON output"),
+    raw: bool = typer.Option(False, "--raw", help="Raw result output"),
 ) -> None:
-    try:
-        raise NotImplementedError("Not Implemented Yet")
-        if json:
-            print_json(data=None)
-        else:
-            rich_inspect(None)
-        raise typer.Exit(0)
-
-    except NotImplementedError as exc:
-        typer.echo(exc)
-        raise typer.Exit(1)
+    data = read_yaml(data_file_name)
+    if json or raw:
+        print_json(data=data)
+    else:
+        data_output = JSON.from_data(data)
+        print(Panel(data_output, title="[yellow]Data", border_style="blue"))
+    raise typer.Exit(0)
 
 
 @app.command()
 def dump(
     ctx: typer.Context,
-    demo: str,
-    json: bool = typer.Option(default=False, help="json output"),
+    data_file_name: typer.FileText = typer.Argument(
+        ...,
+        exists=True,
+        file_okay=True,
+        dir_okay=False,
+        readable=True,
+        resolve_path=True,
+        allow_dash=True,
+        metavar="YAML",
+        help="YAML Data",
+        autocompletion=auto_complete_paths,
+    ),
+    json: bool = typer.Option(False, "--json", help="JSON output"),
+    raw: bool = typer.Option(False, "--raw", help="Raw result output"),
 ) -> None:
-    try:
-        demo_obj = {"a": 1}
-        data = get_members(demo_obj)
-        if json:
-            print_json(data=cleanup_dict(data))
-        else:
-            rich_inspect(demo_obj)
-        raise typer.Exit(0)
-
-    except ValueError as exc:
-        typer.echo(exc)
-        raise typer.Exit(1)
+    data = read_yaml(data_file_name)
+    result = yaml_dump(data).strip()
+    if json:
+        print_json(data={"yaml": result})
+    elif raw:
+        print(result)
+    else:
+        print(
+            Panel(
+                Syntax(
+                    result,
+                    "yaml",
+                    line_numbers=True,
+                    indent_guides=True,
+                ),
+                border_style="blue",
+                title="[yellow]YAML",
+            )
+        )
+    raise typer.Exit(0)
 
 
 if __name__ == "__main__":
--- nettowel/yaml.py
+++ nettowel/yaml.py
@@ -1,4 +1,5 @@
 from typing import IO, Any
+import io
 import ruamel.yaml
 
 
@@ -11,5 +12,21 @@ def load(f: IO[str]) -> Any:
     Returns:
         Any: YAML Data
     """
-    yml = ruamel.yaml.YAML(typ="safe")
-    return yml.load(f)
+    yaml = ruamel.yaml.YAML(typ="safe")
+    return yaml.load(f)
+
+
+def dump(data: Any) -> str:
+    """Dump data as YAML to stream
+
+    Args:
+        data: Data to dump as YAML
+
+    Returns:
+        str: YAML Data
+    """
+    stream = io.StringIO()
+    yaml = ruamel.yaml.YAML(typ="safe")
+    yaml.default_flow_style = False
+    yaml.dump(data=data, stream=stream)
+    return stream.getvalue()
